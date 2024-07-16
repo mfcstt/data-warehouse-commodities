0d603b38a91ce22aa94db298df2057608b14b110
mfcstt <mahcosta54@gmail.com>
2024-07-16 17:07:16 -0300

    chore: database connection

--- src/test.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-
-DATABASE_URL = "postgresql+psycopg2://postgres123:your_password@postgres.c58cacasojz8.us-east-2.rds.amazonaws.com:5432/your_database?sslmode=require"
-engine = create_engine(DATABASE_URL)
-
-# Testar a conexão
-with engine.connect() as connection:
-    result = connection.execute("SELECT 1")
-    print(result.fetchone())
